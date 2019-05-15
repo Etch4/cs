79236fb5cd587ff2be8ba9295e9d032d9b6d46da
Etch4 <pws433a@hotmail.com>
2019-05-15 16:41:15 +0200

    go

---
+++ client.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3.6
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = "192.168.100.50"
+port = 5150
+
+server.connect((host, port))
+data = server.recv(1024)
+print(bytes.decode(data))
+
+while True:
+    data = input("Txt Pls : ")
+    server.send(str.encode(data))
+    data = server.recv(1024)
+    print("Recv from Server : ", bytes.decode(data))
+    if(bytes.decode(data) == "exit"):
+        break
+
+print("Conn is Closing ...")
+server.close()
+
+      
